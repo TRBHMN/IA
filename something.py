47a250c051aaec28889e67e42b119279a8b638d1
TRBHMN <23behmen_t@aswarsaw.org>
2023-02-13 20:56:29 +0100

    Update something.py

--- something.py
+++ something.py
@@ -191,7 +191,7 @@ result = st.button("Click here to make the graph")
 if result:
     if pick3 == "Bar graph":
         st.write('Bar graph, ', x_axis, " vs, ", y_axis)
-        pro = itemxprofit.groupby([x_axis])[y_axis].to_frame()
+        pro = itemxprofit.groupby([x_axis])[y_axis].sum().to_frame()
         st.bar_chart(data=pro, x = index, y= [y_axis])
     if pick3 == "Line Grap67]h":
         st.line_chart(data=itemxprofit, x = [x_axis], y= [y_axis])
