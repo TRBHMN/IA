a4ad77d3ba954c4d7a9a07ddf4f2f17869c61af4
TRBHMN <23behmen_t@aswarsaw.org>
2023-02-14 12:41:00 +0100

    3

--- something.py
+++ something.py
@@ -7,7 +7,6 @@ import pymysql.cursors
 import datetime 
 sns.set(rc={'figure.figsize':(10,5)})
 
-
 lis = []
 lis2= []
 # Connect to the database
@@ -101,10 +100,26 @@ for index, rows in t.iterrows():
     totalprofit = totalprofit + t.loc[index,'profit']
 
 
+# ANALYSIS
+
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+def remote_css(url):
+    st.markdown(f'<link href="{url}" rel="stylesheet">', unsafe_allow_html=True)    
+
+def icon(icon_name):
+    st.markdown(f'<i class="material-icons">{icon_name}</i>', unsafe_allow_html=True)
+
+local_css("style.css")
+remote_css('https://fonts.googleapis.com/icon?family=Material+Icons')
+
+icon("search")
+selected = st.text_input("", "Search...")
+button_clicked = st.button("OK")
 
 
-
-# Add a title and intro text
 st.markdown("<h1 style='text-align: center; color: red;'>Data Analysis</h1>", unsafe_allow_html=True)
 st.text('This is a web app to explore purchasing data, and see some visual data of the processes.')
 
